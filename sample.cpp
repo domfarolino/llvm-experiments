2850849d78703b9e930472388d6679d53289df49
Dominic Farolino <domfarolino@gmail.com>
2019-01-02 19:36:34 -0500

    Create AdderFunction with main, and call printf twice now

--- sample.cpp
+++ sample.cpp
@@ -6,23 +6,30 @@
 int main() {
   CodeGen::Setup();
 
-  //////// ADDER.
-  std::vector<std::pair<std::string, AbstractType>> adderArguments = {
-    std::make_pair("left", AbstractType::Double),
-    std::make_pair("right", AbstractType::Double)
-  };
-  CodeGen::CreateFunction("AdderFunction", AbstractType::Double, adderArguments);
-  Value* returnValue = CodeGen::Add(CodeGen::GetVariable("left"), CodeGen::GetVariable("right"), "addUltimateReturn");
-  CodeGen::ReturnFrom("AdderFunction", returnValue);
-
   //////// MAIN.
   std::vector<std::pair<std::string, AbstractType>> mainArguments;
   CodeGen::CreateFunction("main", AbstractType::Integer, mainArguments);
+
+        //////// ADDER.
+        std::vector<std::pair<std::string, AbstractType>> adderArguments = {
+          std::make_pair("left", AbstractType::Double),
+          std::make_pair("right", AbstractType::Double)
+        };
+        CodeGen::CreateFunction("AdderFunction", AbstractType::Double, adderArguments);
+        Value* returnValue = CodeGen::Add(CodeGen::GetVariable("left"), CodeGen::GetVariable("right"), "addUltimateReturn");
+
+        /////// CALL printf.
+        std::vector<Value*> tmpArgs = { CodeGen::ProduceString("Adder returning: %f\n"), returnValue };
+        CodeGen::CallFunction("printf", tmpArgs, "callprintf");
+
+        CodeGen::ReturnFrom("AdderFunction", returnValue);
+        //////// END ADDER.
+
   Value* adderReturn = CodeGen::CallFunction("AdderFunction", {CodeGen::ProduceNumber(38), CodeGen::ProduceNumber(42)}, "calladder");
   Value* castToInt = CodeGen::CastFloatToInt(adderReturn, "main", "fpToIntegerConv");
 
   /////// CALL printf.
-  std::vector<Value*> printfArguments = { CodeGen::ProduceString("dominicfarolino!!%d"), CodeGen::ProduceInteger(40) };
+  std::vector<Value*> printfArguments = { CodeGen::ProduceString("dominicfarolino!!%d"), castToInt };
   CodeGen::CallFunction("printf", printfArguments, "callprintf");
 
   CodeGen::ReturnFrom("main", castToInt);
--- sample.ll
+++ sample.ll
@@ -1,20 +1,22 @@
 ; ModuleID = 'Dom Sample'
 source_filename = "Dom Sample"
 
-@0 = private unnamed_addr constant [20 x i8] c"dominicfarolino!!%d\00"
+@0 = private unnamed_addr constant [21 x i8] c"Adder returning: %f\0A\00"
+@1 = private unnamed_addr constant [20 x i8] c"dominicfarolino!!%d\00"
 
 declare i32 @printf(i8*, ...)
-
-define double @AdderFunction(double %left, double %right) {
-entry:
-  %addUltimateReturn = fadd double %left, %right
-  ret double %addUltimateReturn
-}
 
 define i32 @main() {
 entry:
   %calladder = call double @AdderFunction(double 3.800000e+01, double 4.200000e+01)
   %fpToIntegerConv = fptosi double %calladder to i32
-  %callprintf = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([20 x i8], [20 x i8]* @0, i32 0, i32 0), i32 40)
+  %callprintf = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([20 x i8], [20 x i8]* @1, i32 0, i32 0), i32 %fpToIntegerConv)
   ret i32 %fpToIntegerConv
+}
+
+define double @AdderFunction(double %left, double %right) {
+entry:
+  %addUltimateReturn = fadd double %left, %right
+  %callprintf = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([21 x i8], [21 x i8]* @0, i32 0, i32 0), double %addUltimateReturn)
+  ret double %addUltimateReturn
 }
